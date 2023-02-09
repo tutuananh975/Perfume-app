8a64a7a2c7c591aa504b41a69f75dd14f3d0c7b3
NgCan <118377905+Decan209@users.noreply.github.com>
2023-02-09 17:39:00 +0700

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import HomePage from './HomePage';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Perfume app
-    </h1>
+    // <h1 className="text-3xl font-bold underline">
+    //   Perfume app
+    // </h1>
+    <HomePage/>
   );
 }
 
---
+++ src/HomePage/data/Brand.tsx
@@ -0,0 +1,34 @@
+export const BrandData = [
+    {
+        id : 1,
+        img : 'https://marvel-b1-cdn.bc0a.com/f00000000246772/cdn.shopify.com/s/files/1/0269/7763/2389/collections/Giorgio_Armani_Brand_Logo_160x.jpg?v=1631795069160px'
+    },
+    {
+        id : 2,
+        img : 'https://marvel-b1-cdn.bc0a.com/f00000000246772/cdn.shopify.com/s/files/1/0269/7763/2389/collections/Azzardo_160x.png?v=1631800512160px'
+    },
+    {
+        id : 3,
+        img : 'https://marvel-b1-cdn.bc0a.com/f00000000246772/cdn.shopify.com/s/files/1/0269/7763/2389/collections/dolce_gabbana_160x.jpg?v=1636379248160px'
+    },
+    {
+        id : 4,
+        img : 'https://marvel-b1-cdn.bc0a.com/f00000000246772/cdn.shopify.com/s/files/1/0269/7763/2389/collections/Carolina_Herrera_Brand_Logo_160x.jpg?v=1636380716160px'
+    },
+    {
+        id : 5,
+        img : 'https://marvel-b1-cdn.bc0a.com/f00000000246772/cdn.shopify.com/s/files/1/0269/7763/2389/collections/Carolina_Herrera_Brand_Logo_160x.jpg?v=1636380716160px'
+    },
+    {
+        id : 6,
+        img : 'https://marvel-b1-cdn.bc0a.com/f00000000246772/cdn.shopify.com/s/files/1/0269/7763/2389/collections/Gucci_Brand_Logo_160x.jpg?v=1631795086160px'
+    },
+    {
+        id : 7,
+        img : 'https://marvel-b1-cdn.bc0a.com/f00000000246772/cdn.shopify.com/s/files/1/0269/7763/2389/collections/Calvin_Klein_Brand_Logo_160x.jpg?v=1631795036160px'
+    },
+    {
+        id : 8,
+        img : 'https://marvel-b1-cdn.bc0a.com/f00000000246772/cdn.shopify.com/s/files/1/0269/7763/2389/collections/Burberry_Brand_Logo_160x.jpg?v=1631795026160px'
+    },
+]
