62c333ba2365adebb531fe3c1572286d165c25cf
Chinorra <ffqbestyasuo1603@gmail.com>
2023-02-17 10:40:08 +0700

    merge

--- src/components/layouts/MainLayout/index.tsx
+++ src/components/layouts/MainLayout/index.tsx
@@ -13,8 +13,8 @@ const MainLayout: FC<PropOutlet> = ({ children } ) => {
         <div>
             <Header />
             <div className="title-products"></div>
-            <div className="container px-2">
-                <div className="grid grid-cols-12 gap-4">
+            <div className="px-2">
+                <div className="grid grid-cols-12 gap-4 justify-between">
                     <div className="col-span-3">
                         <SideBar />
                     </div>
