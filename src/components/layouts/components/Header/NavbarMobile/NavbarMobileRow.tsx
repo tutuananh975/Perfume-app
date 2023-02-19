42bf7035e51136381c6ebfda1332eaa7ef827eee
TuanAnh <tutuananh975@gmail.com>
2023-02-19 17:42:29 +0700

    navbar mobile UI

--- src/components/layouts/components/Header/NavbarMobile/NavbarMobileRow.tsx
+++ src/components/layouts/components/Header/NavbarMobile/NavbarMobileRow.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import Image from '../../../../Image';
 import { FaChevronRight } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 
 
 interface PropNavbarMobileRow {
@@ -12,13 +13,15 @@ interface PropNavbarMobileRow {
 const NavbarMoblieRow: FC<PropNavbarMobileRow> = ({ alt, src, title }) => {
 
     return (
-        <div className='py-4 pr-2 flex justify-between items-center bg-slate-100'>
-            <div className='flex items-center'>
-                <Image alt={alt} src={src} w="43px" h="43px"/>
-                <h4 className='ml-4'>{title}</h4>
+        <NavLink to={alt}>
+            <div className='py-4 pr-2 flex justify-between items-center bg-slate-100 cursor-pointer hover:bg-slate-300'>
+                <div className='flex items-center'>
+                    {<Image alt={alt} src={src} w="43px" h="43px"/>}
+                    <h4 className='ml-4'>{title}</h4>
+                </div>
+                <FaChevronRight className='text-gray-300 mr-6'/>
             </div>
-            <FaChevronRight className='text-gray-300 mr-6'/>
-        </div>
+        </NavLink>
     )
 }
 
--- src/components/layouts/components/Header/NavbarMobile/index.tsx
+++ src/components/layouts/components/Header/NavbarMobile/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import CloseBtn from '../../../../overflay/CloseBtn.tsx';
 import Overflay from '../../../../overflay/Overflay';
+import { FaUserAlt, FaChevronRight } from "react-icons/fa";
 import NavbarMoblieRow from './NavbarMobileRow';
 
 interface PropNavbarMobile {
@@ -23,10 +24,50 @@ const NavbarMobile: FC<PropNavbarMobile> = ({ onCloseNavbarMobile }) => {
                     <h3>MENU</h3>
                     <CloseBtn onClose={onCloseNavbarMobile}/>
                 </div>
+                <div className='flex items-center py-4 pr-2 bg-red-300 cursor-pointer'>
+                    <div 
+                        style={{
+                            width: "43px",
+                            height: "43px",
+                            borderRadius: "50%",
+                            backgroundColor: "grey",
+                            display: "flex"
+                        }}
+                    >
+                        <FaUserAlt className='m-auto'/>
+                    </div>
+                    <p className='ml-4'>My Acount</p>
+                    <FaChevronRight className='text-gray-300 mr-6 ml-auto'/>
+                </div>
                 <NavbarMoblieRow  
                     alt='Women' 
                     src='https://cdn.shopify.com/s/files/1/0269/7763/2389/files/women_icon_47eda231-6206-4341-8b62-dfdce31ca056.png'
                     title="Women's Perfume"
+                />
+                <NavbarMoblieRow  
+                    alt='Men' 
+                    src='https://cdn.shopify.com/s/files/1/0269/7763/2389/files/men_icon_3fcd8ed9-2d2f-4dd0-b094-f0f56d3f2160.png'
+                    title="Men's Perfume"
+                />
+                <NavbarMoblieRow  
+                    alt='best-sellers' 
+                    src='https://cdn.shopify.com/s/files/1/0269/7763/2389/files/clearnace_icon_1136ba11-08b4-46e1-8a54-7b56ef389066.png'
+                    title="Best Sellers"
+                />
+                <NavbarMoblieRow  
+                    alt='gift' 
+                    src='https://cdn.shopify.com/s/files/1/0269/7763/2389/files/Gifts_menu.png?v=1636382258'
+                    title="Gift Sets"
+                />
+                <NavbarMoblieRow  
+                    alt='brands' 
+                    src='https://cdn.shopify.com/s/files/1/0269/7763/2389/files/atoz.png'
+                    title="Shop by brands"
+                />
+                <NavbarMoblieRow  
+                    alt='fragrance' 
+                    src='https://cdn.shopify.com/s/files/1/0269/7763/2389/files/women_icon_47eda231-6206-4341-8b62-dfdce31ca056.png'
+                    title="Join Theo Club"
                 />
             </div>
             <Overflay onClose={onCloseNavbarMobile}/>
