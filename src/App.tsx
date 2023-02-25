db9a4cf73dd692ddf369f9d74d5d3b1fdc0373ee
NgCan <118377905+Decan209@users.noreply.github.com>
2023-02-25 16:44:25 +0700

    VV

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter, redirect, Navigate } from 'react-router-dom';
 import './App.css';
 import { MainLayout } from './components/layouts';
 import { publicRoutes } from './routes';
 import ProductDetail from './pages/ProductDetail';
+import Help from './pages/Help';
+import { useSelector } from 'react-redux';
+import HomePage from './pages/HomePage';
 
-function App() {
+
+function App() {  
+  
+
+
+  const PrivateRoutes = () => {
+    return localStorage.getItem("accessToken") ? <Help/> : <Navigate to='/' />
+  }
+
   return (
     <HashRouter>
       <div className='App'>
@@ -17,6 +28,7 @@ function App() {
               return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />
             })
           }
+          <Route path="/help" element={<PrivateRoutes/>} />
         </Routes>
       </div>
     </HashRouter>
